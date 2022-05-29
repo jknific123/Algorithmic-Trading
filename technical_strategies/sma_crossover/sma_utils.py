ad9580925d7d5c5a5abeb15bc952d6f4fde06b2b
jknfic <jk2922@student.uni-lj.si>
2022-05-29 21:34:42 +0200

    Nic pomembnega, testiranje novega pristopa iteriranja

--- technical_strategies/sma_crossover/sma_utils.py
+++ technical_strategies/sma_crossover/sma_utils.py
@@ -9,10 +9,15 @@ range1 = pd.date_range('2005-11-21', '2016-5-21')
 # range2 = pd.date_range('2021-10-6','2021-10-11')
 
 # range1 = ["2021-10-01", "2021-10-02", "2021-11-03", "2021-10-04"]
-range2 = ["2021-10-01", "2021-10-02", "2021-10-04", "2021-10-03", "2021-10-04", "2021-10-04"]
+# range2 = ["2021-10-01", "2021-10-02", "2021-10-04", "2021-10-03", "2021-10-04", "2021-10-04"]
 
-df1 = pd.DataFrame(1, columns=['value', 'apple', 'orange'], index=range2)
-df2 = pd.DataFrame(1, columns=['value', 'apple', 'orange'], index=range2)
+dictToDf = {'Date': ["2021-10-01", "2021-10-02", "2021-10-04", "2021-10-03", "2021-10-04", "2021-10-04"],
+            'value': [1, 1, 1, 1, 1, 1],
+            'apple': [1, 1, 1, 1, 1, 1],
+            'orange': [1, 1, 1, 1, 1, 1]}
+
+df1 = pd.DataFrame(data=dictToDf)
+# df2 = pd.DataFrame(1, columns=['value', 'apple', 'orange'], index=range2)
 
 display(df1)
 # print('df1: ', df1)
@@ -31,6 +36,17 @@ for i in range(0, 30):
         counter += 1
 print(counter)
 print(datetime.datetime.now() - begin_time)
+
+print('testing itteration drugacen dostop: ')
+begin_time10 = datetime.datetime.now()
+counter10 = 0
+for k in range(0, 30):
+    for x in range(0, len(df1)):
+        # print(df1['value'].iat[x])
+        df1['value'].to_numpy()[x] = df1['value'].to_numpy()[x] + df1['apple'].to_numpy()[x]
+        counter10 += 1
+print(counter10)
+print(datetime.datetime.now() - begin_time10)
 
 print('testiram itterrows')
 begin_time2 = datetime.datetime.now()
@@ -55,20 +71,13 @@ print(counter)
 print(datetime.datetime.now() - begin_time3)
 
 
-def preveriPravilnostDatumov(data):
-    dictDF = data.to_dict('records')
-    index = 0
-    for record in dictDF:
-        if index != 0:
-            None
-        print(record)
-        index += 1
 
 
-stocksDB = getStocks.StockOHLCData()
 
-testDF = stocksDB.getCompanyStockDataInRange("2005-11-21", "2005-11-28", "AAPL")
-print()
-display(testDF)
+# stocksDB = getStocks.StockOHLCData()
+#
+# testDF = stocksDB.getCompanyStockDataInRangeTabela("2005-11-21", "2005-11-28", "AAPL")
+# print()
+# display(testDF)
 
 # preveriPravilnostDatumov(df1)
