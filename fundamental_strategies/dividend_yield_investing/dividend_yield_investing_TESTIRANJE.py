87829c2aa9f62988113dee12ad0dba7c0acbd70a
jknfic <jk2922@student.uni-lj.si>
2022-07-10 16:14:06 +0200

    Manjsi popravek testiranje dividned investing

--- fundamental_strategies/dividend_yield_investing/dividend_yield_investing_TESTIRANJE.py
+++ fundamental_strategies/dividend_yield_investing/dividend_yield_investing_TESTIRANJE.py
@@ -25,6 +25,6 @@ print('value investing strategy po klicu inicializacije objekta')
 testirajNaPortfoliu(start_date="2005-11-21", end_date="2016-05-21", dowTickers=dowJonesIndexData, stock_prices_db=stockPricesDB, fundamental_indicators=fundamentalIndicatorsDB)
 
 # testna mnozica
-# testirajNaPortfoliu(start_date="2016-05-21", end_date="2021-01-01", dowTickers=dowJonesIndexData, stock_prices_db=stockPricesDB, fundamental_indicators=fundamental_data)
+# testirajNaPortfoliu(start_date="2016-05-21", end_date="2021-01-01", dowTickers=dowJonesIndexData, stock_prices_db=stockPricesDB, fundamental_indicators=fundamentalIndicatorsDB)
 
 print('KONEC!!! ', datetime.datetime.now() - begin_time)
