0db5f3c83503daf2fc6dd6fed415d7fed24902e1
jknfic <jk2922@student.uni-lj.si>
2022-07-29 13:30:32 +0200

    Popravek testiranja in rezultati stohastičnega oscilatorja 70% - 30%

--- technical_strategies/sthohastic_oscilator/stohastic_oscilator_testiranje_portfelj.py
+++ technical_strategies/sthohastic_oscilator/stohastic_oscilator_testiranje_portfelj.py
@@ -8,6 +8,7 @@ from utility import utils as util
 def najdiOptimalneParametreNaPotrfoliu(start_period, end_period, dowTickers, stock_prices_db, hold_obdobje):
     print("Testiram na ucni mnozici")
     ucni_rezultati = {}
+    ucni_zacetna_investicijat = {}
     K_dolzina_vrednost = [5, 9, 15, 20, 25]
     D_dolzina_vrednosti = [3, 5, 8, 11, 14]
     for k_sma in K_dolzina_vrednost:  # 210
@@ -16,17 +17,22 @@ def najdiOptimalneParametreNaPotrfoliu(start_period, end_period, dowTickers, sto
                 ucni_rezultati[f"[{k_sma},{d_sma}]"] = {}
                 print(f"Kombinacija: K_SMA = {k_sma} , %D = {d_sma}")
                 rezultati_backtesta = backtest(start=start_period, end=end_period, high_low_period=k_sma, d_sma_period=d_sma, dowTickers=dowTickers, stockPricesDB=stock_prices_db, hold_obdobje=hold_obdobje)
-                ucni_rezultati[f"[{k_sma},{d_sma}]"] = rezultati_backtesta['Total'].to_numpy()[-1]
+                ucni_rezultati[f"[{k_sma},{d_sma}]"] = rezultati_backtesta['totals']['Total'].to_numpy()[-1]
+                ucni_zacetna_investicijat[f"[{k_sma},{d_sma}]"] = rezultati_backtesta['zacetna investicija']
 
-    return ucni_rezultati
+    ucni_return_dict = {'ucni_rezultati': ucni_rezultati, 'ucni_zacetna_investicijat': ucni_zacetna_investicijat}
+    return ucni_return_dict
 
 
 def testirajNaPortfoliuUcnaMnozica(dowTickers, stock_prices_db, hold_cas):
     zacetni_cas = datetime.datetime.now()
     # pridobimo dict key: kombinacija, value: koncno stanje sredstev
-    rez_total_ucni = najdiOptimalneParametreNaPotrfoliu(start_period="2005-11-21", end_period="2017-02-02", dowTickers=dowTickers, stock_prices_db=stock_prices_db,
+    return_ucni = najdiOptimalneParametreNaPotrfoliu(start_period="2005-11-21", end_period="2017-02-02", dowTickers=dowTickers, stock_prices_db=stock_prices_db,
                                                         hold_obdobje=hold_cas)
-    print("Koncal testiranej na ucni: ", datetime.datetime.now() - begin_time)
+
+    rez_total_ucni = return_ucni['ucni_rezultati']
+    rez_zacetna_investicija_ucni = return_ucni['ucni_zacetna_investicijat']
+    print("Koncal testiranej na ucni: ", datetime.datetime.now() - zacetni_cas)
 
     # sortiram rezultate po velikosti koncnega stanja sredstev
     sorted_rez_total_ucni = {k: v for k, v in sorted(rez_total_ucni.items(), key=lambda item: item[1])}
@@ -36,7 +42,9 @@ def testirajNaPortfoliuUcnaMnozica(dowTickers, stock_prices_db, hold_cas):
         for x in sorted_rez_total_ucni:
             print(x, ": ", sorted_rez_total_ucni[x])
             row_string = str(x) + ': ' + str(round(sorted_rez_total_ucni[x], 2)) + ' ' + str(
-                util.povprecnaLetnaObrestnaMera(30000, sorted_rez_total_ucni[x], util.ucnaMnozicaLeta())) + '%'
+                util.povprecnaLetnaObrestnaMera(
+                    rez_zacetna_investicija_ucni[x], sorted_rez_total_ucni[x], util.ucnaMnozicaLeta())) + '% ' + str(round(rez_zacetna_investicija_ucni[x], 2))
+
             f.write(row_string)
             f.write('\n')
         f.write('hold_obdobje: ' + str(hold_cas))
@@ -51,10 +59,10 @@ def najdiOptimalneParametreNaPotrfoliuZaHoldObdobjaUcnaMnozica(hold_obdobja_list
 
 
 def testirajOptimalneNaTestniMnoziciZaHoldObdobja(dowTickers, testnaStockPricesDB, hold_obdobja_list):
-    optimalni_dnevni = [[20, 14], [15, 11], [15, 14]]
-    optimalni_tedenski = [[20, 14], [15, 11], [15, 14]]
+    optimalni_dnevni = [[15, 11], [20, 14], [15, 14]]
+    optimalni_tedenski = [[15, 11], [20, 14], [15, 14]]
     optimalni_mesecni = [[20, 14], [15, 11], [15, 14]]
-    optimalni_letni = [[25, 3], [20, 5], [5, 3]]
+    optimalni_letni = [[20, 5], [25, 5], [5, 3]]
     dict_parametrov = {1: optimalni_dnevni, 7: optimalni_tedenski, 31: optimalni_mesecni, 365: optimalni_letni}
 
     for hold_cas in hold_obdobja_list:
@@ -63,18 +71,24 @@ def testirajOptimalneNaTestniMnoziciZaHoldObdobja(dowTickers, testnaStockPricesD
         hold_obdobje_string = util.getStringForHoldObdobje(hold_cas)
         print('Testiram za hold obdobje: ' + hold_obdobje_string, hold_cas)
         rez_total_testni = {}
+        rez_zacetna_investicija_testni = {}
         zacetni_cas = datetime.datetime.now()
         for kombinacija in trenutni_parametri_list:
             print('Kombinacija: ', kombinacija)
             temp = backtest('2017-02-02', '2021-11-21', kombinacija[0], kombinacija[1], dowTickers, testnaStockPricesDB, hold_cas)
-            koncno_stanje = temp['Total'].to_numpy()[-1]
+            koncno_stanje = temp['totals']['Total'].to_numpy()[-1]
+            zacetna_investicija = temp['zacetna investicija']
             rez_total_testni[f"[{kombinacija[0]},{kombinacija[1]}]"] = koncno_stanje
+            rez_zacetna_investicija_testni[f"[{kombinacija[0]},{kombinacija[1]}]"] = zacetna_investicija
 
         sorted_rez_total_testni = {k: v for k, v in sorted(rez_total_testni.items(), key=lambda item: item[1])}
         with open(f'D:\Faks\Algorithmic-Trading\/technical_strategies\/sthohastic_oscilator\/Stohastic_rezultati_testna\/stohastic_oscilator_{hold_obdobje_string}_testna.txt', 'w', encoding='UTF8') as f:
             for x in sorted_rez_total_testni:
                 print(x, ': ', sorted_rez_total_testni[x], ' ', util.testnaMnozicaLeta())
-                row_string = str(x) + ': ' + str(round(sorted_rez_total_testni[x], 2)) + ' ' + str(util.povprecnaLetnaObrestnaMera(30000, sorted_rez_total_testni[x], util.testnaMnozicaLeta())) + '%'
+                row_string = str(x) + ': ' + str(round(sorted_rez_total_testni[x], 2)) + ' ' + str(
+                    util.povprecnaLetnaObrestnaMera(rez_zacetna_investicija_testni[x], sorted_rez_total_testni[x], util.testnaMnozicaLeta())) + '% ' + str(
+                    round(rez_zacetna_investicija_testni[x], 2))
+
                 f.write(row_string)
                 f.write('\n')
             f.write('hold_obdobje: ' + str(hold_cas))
@@ -107,7 +121,7 @@ print('Stohastic oscilator strategy portfelj, inicializacije objekta')
 # najdiOptimalneParametreNaPotrfoliuZaHoldObdobjaUcnaMnozica(hold_obdobja_list=list_hold_obdobja_portelj,
 #                                                            dowJonesIndexDataPortfelj=dowJonesIndexData, stockPricesDBPortfelj=stockPricesDB)
 
-# testirajOptimalneNaTestniMnoziciZaHoldObdobja(dowTickers=dowJonesIndexData, testnaStockPricesDB=stockPricesDB, hold_obdobja_list=list_hold_obdobja_portelj)
+testirajOptimalneNaTestniMnoziciZaHoldObdobja(dowTickers=dowJonesIndexData, testnaStockPricesDB=stockPricesDB, hold_obdobja_list=list_hold_obdobja_portelj)
 
 # preverjanje uspesnosti optimalnih kombinacij na celotnem casovnem obdobju
 # testirajNaPortfoliuEnoKombinacijo(start_date="2005-11-21", end_date="2021-11-21", k_sma=25, d_sma=3,
